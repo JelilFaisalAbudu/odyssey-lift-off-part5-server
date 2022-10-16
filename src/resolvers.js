fff3eff34591a2909b23af4c6be207c180aa6745
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2022-10-16 04:46:53 +0000

    Deprecate Track and Module length properties. Use durationInSeconds instead

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,10 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInseconds: ({ length }) => length,
+  },
+  Module: {
+    durationInseconds: ({ length }) => length,
   },
 };
 
